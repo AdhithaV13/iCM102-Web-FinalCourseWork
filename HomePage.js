4ff606b3f858e51a0eefd8c7899009c91545b4b9
Adhitha Vithanage <130221684+AdhithaV13@users.noreply.github.com>
2023-09-23 23:41:09 +0530

    Add files via upload

--- HomePage.js
+++ HomePage.js
@@ -1,12 +1,35 @@
+defaultValues();
+
 async function btnSearchClicked(){
     const apiKey = "44c917daebe44e7dbf3153750231409";
-    const urlCurrent = "http://api.weatherapi.com/v1/current.json?key";
-    const city = document.getElementById("txtSearch").value;
-
-    console.log(city);
+    const urlCurrent = "http://api.weatherapi.com/v1/current.json?key=";
+    let city = document.getElementById('txtSearch').value;
         
     const response=await fetch(urlCurrent+`${apiKey}&q=`+city);
     var data=await response.json();
 
     console.log(data);
+
+    document.getElementById("valuesOfTemperature1").innerHTML = data.current.temp_c+" °C"+"<br>"+data.current.temp_f+" °F"+"<br>"+Math.round(data.current.temp_c+273.15)+" K";
+    document.getElementById("valuesOfHumidity1").innerHTML = data.current.humidity+"%";
+    document.getElementById("valuesOfPrecipition1").innerHTML = data.current.precip_mm+" mm"+"<br>"+data.current.precip_in+" inch";
+    document.getElementById("valuesOfWindSpeed1").innerHTML = data.current.wind_kph+" kph"+"<br>"+data.current.wind_mph+" mph";
+    document.getElementById("conditionIcon1").src = data.current.condition.icon;
+    document.getElementById("conditionText1").innerHTML = data.current.condition.text;
+}
+
+async function defaultValues(){
+    const apiKey = "44c917daebe44e7dbf3153750231409";
+    const urlCurrent = "http://api.weatherapi.com/v1/current.json?key=";
+    let city = "Colombo";
+        
+    const response=await fetch(urlCurrent+`${apiKey}&q=`+city);
+    var data=await response.json();
+
+    document.getElementById("valuesOfTemperature1").innerHTML = data.current.temp_c+" °C"+"<br>"+data.current.temp_f+" °F"+"<br>"+Math.round(data.current.temp_c+273.15)+" K";
+    document.getElementById("valuesOfHumidity1").innerHTML = data.current.humidity+"%";
+    document.getElementById("valuesOfPrecipition1").innerHTML = data.current.precip_mm+" mm"+"<br>"+data.current.precip_in+" inch";
+    document.getElementById("valuesOfWindSpeed1").innerHTML = data.current.wind_kph+" kph"+"<br>"+data.current.wind_mph+" mph";
+    document.getElementById("conditionIcon1").src = data.current.condition.icon;
+    document.getElementById("conditionText1").innerHTML = data.current.condition.text;
 }
