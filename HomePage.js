15eef01678508ff48b5db0a22aa116252716e725
Adhitha Vithanage <130221684+AdhithaV13@users.noreply.github.com>
2023-09-24 23:05:20 +0530

    Add files via upload

--- HomePage.js
+++ HomePage.js
@@ -9,8 +9,6 @@ async function btnSearchClicked(){
     const response=await fetch(urlCurrent+`${apiKey}&q=`+city);
     var data=await response.json();
 
-    console.log(data);
-
     document.getElementById("valuesOfTemperature1").innerHTML = data.current.temp_c+" °C"+"<br>"+data.current.temp_f+" °F"+"<br>"+Math.round(data.current.temp_c+273.15)+" K";
     document.getElementById("valuesOfHumidity1").innerHTML = data.current.humidity+"%";
     document.getElementById("valuesOfPrecipition1").innerHTML = data.current.precip_mm+" mm"+"<br>"+data.current.precip_in+" inch";
@@ -20,25 +18,46 @@ async function btnSearchClicked(){
     //-------------------------------------------------------------------------
 }
 
+async function txtDateEntered1(){
+    //-------------------Historical Weather------------------------
+    const apiKey2 = "44c917daebe44e7dbf3153750231409";
+    const urlHistorical = "http://api.weatherapi.com/v1/history.json?key=";
+    let city2 = document.getElementById("txtSearch").value;
+    let date1 = document.getElementById("txtDate1").value;
+
+    const response2=await fetch(urlHistorical+`${apiKey2}&q=`+city2+`&dt=`+date1);
+    var data2=await response2.json();
+
+    console.log(data2);
+
+    document.getElementById("valuesOfTemperature3").innerHTML = data2.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data2.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(data2.forecast.forecastday[0].day.avgtemp_c+273.15)+" K";
+    document.getElementById("valuesOfHumidity3").innerHTML = data2.forecast.forecastday[0].day.avghumidity+"%";
+    document.getElementById("valuesOfPrecipition3").innerHTML = data2.forecast.forecastday[0].day.totalprecip_mm+" mm"+"<br>"+data2.forecast.forecastday[0].day.totalprecip_in+" inch";
+    document.getElementById("valuesOfWindSpeed3").innerHTML = data2.forecast.forecastday[0].day.maxwind_mph+" mph"+"<br>"+data2.forecast.forecastday[0].day.maxwind_kph+" kph";
+    document.getElementById("conditionIcon3").src = data2.forecast.forecastday[0].day.condition.icon;
+    document.getElementById("conditionText3").innerHTML = data2.forecast.forecastday[0].day.condition.text;
+    //---------------------------------------------------------------------
+}
+
 async function txtDateEntered(){
-    //---------------------Historical Weather--------------------
+    //-------------------Future Weather------------------------
     const apiKey1 = "44c917daebe44e7dbf3153750231409";
-    const urlCurrent1 = "http://api.weatherapi.com/v1/history.json?key=";
+    const urlFuture = "http://api.weatherapi.com/v1/future.json?key=";
     let city1 = document.getElementById("txtSearch").value;
     let date = document.getElementById("txtDate").value;
 
-    const response1=await fetch(urlCurrent1+`${apiKey1}&q=`+city1+`&dt=`+date);
+    const response1=await fetch(urlFuture+`${apiKey1}&q=`+city1+`&dt=`+date);
     var data1=await response1.json();
 
     console.log(data1);
 
-    document.getElementById("valuesOfTemperature2").innerHTML = data1.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data1.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(276.15+(data1.forecast.forecastday[0].day.avgtemp_c))+" K";
+    document.getElementById("valuesOfTemperature2").innerHTML = data1.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data1.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(data1.forecast.forecastday[0].day.avgtemp_c+273.15)+" K";
     document.getElementById("valuesOfHumidity2").innerHTML = data1.forecast.forecastday[0].day.avghumidity+"%";
     document.getElementById("valuesOfPrecipition2").innerHTML = data1.forecast.forecastday[0].day.totalprecip_mm+" mm"+"<br>"+data1.forecast.forecastday[0].day.totalprecip_in+" inch";
-    document.getElementById("valuesOfWindSpeed2").innerHTML = data1.forecast.forecastday[0].day.maxwind_kph+" kph"+"<br>"+data1.forecast.forecastday[0].day.maxwind_mph+" mph"
+    document.getElementById("valuesOfWindSpeed2").innerHTML = data1.forecast.forecastday[0].day.maxwind_mph+" mph"+"<br>"+data1.forecast.forecastday[0].day.maxwind_kph+" kph";
     document.getElementById("conditionIcon2").src = data1.forecast.forecastday[0].day.condition.icon;
     document.getElementById("conditionText2").innerHTML = data1.forecast.forecastday[0].day.condition.text;
-    //------------------------------------------------------------
+    //-----------------------------------------------------------
 }
 
 async function defaultValues(){
@@ -50,6 +69,8 @@ async function defaultValues(){
     const response=await fetch(urlCurrent+`${apiKey}&q=`+city);
     var data=await response.json();
 
+    console.log(data);
+
     document.getElementById("valuesOfTemperature1").innerHTML = data.current.temp_c+" °C"+"<br>"+data.current.temp_f+" °F"+"<br>"+Math.round(data.current.temp_c+273.15)+" K";
     document.getElementById("valuesOfHumidity1").innerHTML = data.current.humidity+"%";
     document.getElementById("valuesOfPrecipition1").innerHTML = data.current.precip_mm+" mm"+"<br>"+data.current.precip_in+" inch";
@@ -58,22 +79,41 @@ async function defaultValues(){
     document.getElementById("conditionText1").innerHTML = data.current.condition.text;
     //-----------------------------------------------------------
 
-    //---------------------Historical Weather--------------------
+    //-------------------Future Weather------------------------
     const apiKey1 = "44c917daebe44e7dbf3153750231409";
-    const urlCurrent1 = "http://api.weatherapi.com/v1/history.json?key=";
+    const urlFuture = "http://api.weatherapi.com/v1/future.json?key=";
     let city1 = "Colombo";
-    let date = "2023-09-20";
+    let date = "2023-10-25";
 
-    const response1=await fetch(urlCurrent1+`${apiKey1}&q=`+city1+`&dt=`+date);
+    const response1=await fetch(urlFuture+`${apiKey1}&q=`+city1+`&dt=`+date);
     var data1=await response1.json();
 
     console.log(data1);
 
-    document.getElementById("valuesOfTemperature2").innerHTML = data1.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data1.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(276.15+(data1.forecast.forecastday[0].day.avgtemp_c))+" K";
+    document.getElementById("valuesOfTemperature2").innerHTML = data1.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data1.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(data1.forecast.forecastday[0].day.avgtemp_c+273.15)+" K";
     document.getElementById("valuesOfHumidity2").innerHTML = data1.forecast.forecastday[0].day.avghumidity+"%";
     document.getElementById("valuesOfPrecipition2").innerHTML = data1.forecast.forecastday[0].day.totalprecip_mm+" mm"+"<br>"+data1.forecast.forecastday[0].day.totalprecip_in+" inch";
-    document.getElementById("valuesOfWindSpeed2").innerHTML = data1.forecast.forecastday[0].day.maxwind_kph+" kph"+"<br>"+data1.forecast.forecastday[0].day.maxwind_mph+" mph"
+    document.getElementById("valuesOfWindSpeed2").innerHTML = data1.forecast.forecastday[0].day.maxwind_mph+" mph"+"<br>"+data1.forecast.forecastday[0].day.maxwind_kph+" kph";
     document.getElementById("conditionIcon2").src = data1.forecast.forecastday[0].day.condition.icon;
     document.getElementById("conditionText2").innerHTML = data1.forecast.forecastday[0].day.condition.text;
-    //------------------------------------------------------------
+    //-----------------------------------------------------------
+
+    //-------------------Historical Weather------------------------
+    const apiKey2 = "44c917daebe44e7dbf3153750231409";
+    const urlHistorical = "http://api.weatherapi.com/v1/history.json?key=";
+    let city2 = "Colombo";
+    let date1 = "2023-08-25";
+
+    const response2=await fetch(urlHistorical+`${apiKey2}&q=`+city2+`&dt=`+date1);
+    var data2=await response2.json();
+
+    console.log(data2);
+
+    document.getElementById("valuesOfTemperature3").innerHTML = data2.forecast.forecastday[0].day.avgtemp_c+" °C"+"<br>"+data2.forecast.forecastday[0].day.avgtemp_f+" °F"+"<br>"+Math.round(data2.forecast.forecastday[0].day.avgtemp_c+273.15)+" K";
+    document.getElementById("valuesOfHumidity3").innerHTML = data2.forecast.forecastday[0].day.avghumidity+"%";
+    document.getElementById("valuesOfPrecipition3").innerHTML = data2.forecast.forecastday[0].day.totalprecip_mm+" mm"+"<br>"+data2.forecast.forecastday[0].day.totalprecip_in+" inch";
+    document.getElementById("valuesOfWindSpeed3").innerHTML = data2.forecast.forecastday[0].day.maxwind_mph+" mph"+"<br>"+data2.forecast.forecastday[0].day.maxwind_kph+" kph";
+    document.getElementById("conditionIcon3").src = data2.forecast.forecastday[0].day.condition.icon;
+    document.getElementById("conditionText3").innerHTML = data2.forecast.forecastday[0].day.condition.text;
+    //---------------------------------------------------------------------
 }
